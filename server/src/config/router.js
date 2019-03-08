8d19c5becfc7898c24aef1a06cdeb597abf73922
WindStormrage <695729033@qq.com>
2019-03-08 22:16:38 +0800

    添加注册接口

--- server/src/config/router.js
+++ server/src/config/router.js
@@ -1,5 +1,6 @@
 module.exports = [
   ['/uploadImage', 'common/uploadImage'],
   ['/sendMail', 'common/sendMail'],
-  ['/login', 'user/login']
+  ['/login', 'user/login'],
+  ['/register', 'user/register']
 ];
--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,7 +1,56 @@
 const Base = require('./base.js');
 
 module.exports = class extends Base {
+  constructor(...args) {
+    super(...args);
+    this.userModel = this.model('user');
+  }
   loginAction() {
     return this.success({name: 'ok'});
+  }
+  async registerAction() {
+    const data = this.post();
+    // 先看邮件验证码对不对
+    const promise1 = await this.cache(data.email);
+    // 然后看学号密码对应的上吗(暂时不做验证)
+    const promise2 = await Promise.resolve(true);
+    // 然后看学号是否注册过了
+    const promise3 = await this.userModel
+      .where({
+        sid: data.sid
+      })
+      .count();
+    // 然后看用户名是否存在
+    const promise4 = await this.userModel
+      .where({
+        name: data.name
+      })
+      .count();
+    const [code, isStudent, countSid, countName] = await Promise.all([promise1, promise2, promise3, promise4]);
+    if (code !== data.verification) {
+      return this.fail(1001, '邮箱验证码错误，请输入正确的验证码');
+    }
+    if (!isStudent) {
+      return this.fail(1002, '教务系统验证错误，请输入正确的学号或密码');
+    }
+    if (countSid !== 0) {
+      return this.fail(1003, '学号有重复，此学号已经注册过用户');
+    }
+    if (countName !== data.verification) {
+      return this.fail(1004, '用户名有重复，请重新输入用户名');
+    }
+    // 入库
+    await this.userModel.add({
+      avatar: 'http://123.207.39.128:8080/upload/file/ca37bac437e20a6d71d3fdc51e5c00fa',
+      uid: new Date().getTime(),
+      name: data.name,
+      password: data.pass,
+      sid: data.sid,
+      // data.sPass,
+      email: data.email,
+      create_at: '',
+      update_at: '',
+    });
+    return this.success({});
   }
 };
