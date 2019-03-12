cc6e3611787ac754fbce140c326bf1e74329f68a
WindStormrage <695729033@qq.com>
2019-03-12 16:32:34 +0800

    完成登录接口

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -6,8 +6,19 @@ module.exports = class extends Base {
     super(...args);
     this.userModel = this.model('user');
   }
-  loginAction() {
-    return this.success({name: 'ok'});
+  async loginAction() {
+    const data = this.post();
+    const userInfo = await this.userModel
+      .where({
+        name: data.name,
+        password: data.pass
+      })
+      .find();
+    if (think.isEmpty(userInfo)) {
+      return this.fail(1001, '账号或密码错误!');
+    }
+    await this.session('userInfo', userInfo);
+    return this.success(userInfo);
   }
   async registerAction() {
     const data = this.post();
@@ -51,7 +62,7 @@ module.exports = class extends Base {
     }
     const date = formatDateTime(new Date());
     // 入库
-    await this.userModel.add({
+    const id = await this.userModel.add({
       avatar: 'http://123.207.39.128:8080/upload/file/ca37bac437e20a6d71d3fdc51e5c00fa',
       uid: new Date().getTime(),
       name: data.name,
@@ -62,6 +73,9 @@ module.exports = class extends Base {
       create_at: date,
       last_login_at: date
     });
-    return this.success('注册成功');
+    // 注册成功就设置session
+    const userInfo = await this.userModel.where({id}).find();
+    await this.session('userInfo', userInfo);
+    return this.success(userInfo);
   }
 };
