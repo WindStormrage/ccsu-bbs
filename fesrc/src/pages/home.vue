d1c3cab8f004e9c6d62dc15f9a34050689c9a05e
WindStormrage <695729033@qq.com>
2019-03-13 19:39:28 +0800

    完成home页面的渲染

--- fesrc/src/pages/home.vue
+++ fesrc/src/pages/home.vue
@@ -13,6 +13,7 @@
             border: 1px solid #666;
             margin: 20px;
             .title {
+                cursor: pointer;
                 padding: 15px;
                 .msg {
                     display: inline-block;
@@ -35,8 +36,15 @@
                     text-overflow: ellipsis;
                     white-space: nowrap;
                     margin-top: 10px;
+                    cursor: pointer;
                 }
             }
+        }
+        .card-null {
+            height: 285px;
+            width: 400px;
+            border: 1px solid transparent;
+            margin: 20px;
         }
     }
 }
@@ -46,22 +54,20 @@
         <Header :bread-msg="breadMsg"></Header>
         <div class="content">
             <div class="flex">
-                <div class="card" v-for="item in 10" :key="item">
-                    <div class="title">
+                <div class="card" v-for="label in data" :key="label.id">
+                    <div class="title" @click="$router.push(`/list/${label.url}`)">
                         <div class="msg">
-                            <h1>学术交流</h1>
-                            <p>帖子数：50人  留言数：500人</p>
+                            <h1>{{label.name}}</h1>
+                            <p>帖子数：{{label.postCount}}人  跟帖数：{{label.participationCount}}帖</p>
                         </div>
                         <img src="./../assets/right.png">
                     </div>
                     <hr>
                     <ul class="list">
-                        <li>一条消息一条消息一条消息一条消息一条消息一条消息一条消息一条消息</li>
-                        <li>一条消息一条消息</li>
-                        <li>一条消息</li>
-                        <li>一条消息一条消息一条消息一条消息</li>
+                        <li v-for="post in label.posts" :key="post.id" @click="$router.push(`/list/${label.url}/${post.id}`)">{{post.name}}</li>
                     </ul>
                 </div>
+                <div class="card-null" v-for="(item, index) in nullCount" :key="index"></div>
             </div>
         </div>
         <right-msg></right-msg>
@@ -70,6 +76,7 @@
 <script>
 import Header from './../components/header.vue'
 import RightMsg from './../components/rightMsg.vue'
+import Sycn from "./../js/util/sync.js";
 export default {
     components: {
         Header,
@@ -79,8 +86,32 @@ export default {
         return {
             breadMsg: [
                 {name: '首页', route: '/home'}
-            ]
+            ],
+            data: [],
+            nullCount: 0,
         }
-    }
+    },
+    mounted() {
+        this.getData();
+    },
+    methods: {
+        getData() {
+          const sync = new Sycn();
+          sync.GET("/api/home")
+            .then(data => {
+                this.data = data.data;
+                // 一行的数量
+                const lineCount = parseInt((document.body.clientWidth - 410) / 440);
+                this.nullCount = data.data.length % lineCount;
+            })
+            .catch(err => {
+                this.$message({
+                  showClose: true,
+                  message: '登录失败,服务器错误,请稍后重试!',
+                  type: 'error'
+                });
+            });
+        }
+    },
 }
 </script>
